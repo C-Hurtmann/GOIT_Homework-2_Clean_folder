4c78865d717ef96728c0e47a7e76a80b8e5cfd6e
C-Hurtmann <Constantine2903@gmail.com>
2023-01-15 18:29:36 +0200

    Update tool name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 
-setup(name='clean folder',
+setup(name='clean-folder',
       version='0.1.2',
       description='Sort specified folder to subfolders by their suffix (other subfolders will be disposed). Rename files to ASCII format. Unpack the archives',
       url='https://github.com/C-Hurtmann/Clean_folder',
