30f158e90220284915d2c3857a3a3cf7149688f4
C-Hurtmann <Constantine2903@gmail.com>
2023-01-15 19:05:41 +0200

    Add classifiers to setup

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_namespace_packages
 
 
-setup(name='clean-folder',
+setup(name='clean-folder_from_ZCO',
       version='0.1.2',
       description='Sort specified folder to subfolders by their suffix (other subfolders will be disposed). Rename files to ASCII format. Unpack the archives',
       url='https://github.com/C-Hurtmann/Clean_folder',
       license='MIT',
+      classifiers=['Programming Language :: Python :: 3', 
+                  'License :: OSI Approved :: MIT License', 
+                  'Operating System :: OS Independent']
       author='Constantine Zagorodnyi',
       author_email='Constantine2903@gmail.com',
       packages=find_namespace_packages(), 
